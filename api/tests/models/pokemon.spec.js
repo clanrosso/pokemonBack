c90b5b85fb7cb886ab7ea26000d6e3bf613a7dd4
clanrosso <claudiorossocba@hotmail.com.ar>
2022-08-08 23:26:19 -0300

    testingBack

--- api/tests/models/pokemon.spec.js
+++ api/tests/models/pokemon.spec.js
@@ -1,21 +1,52 @@
-const { Pokemon, conn } = require('../../src/db.js');
-const { expect } = require('chai');
+const { Pokemon, conn } = require("../../src/db.js");
+const { expect } = require("chai");
 
-describe('Pokemon model', () => {
-  before(() => conn.authenticate()
-    .catch((err) => {
-      console.error('Unable to connect to the database:', err);
-    }));
-  describe('Validators', () => {
+describe("Pokemon model", () => {
+  before(() =>
+    conn.authenticate().catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    })
+  );
+  describe("Validators", () => {
     beforeEach(() => Pokemon.sync({ force: true }));
-    describe('name', () => {
-      it('should throw an error if name is null', (done) => {
+    describe("pokemon", () => {
+      it("should throw an error if name is null", (done) => {
         Pokemon.create({})
-          .then(() => done(new Error('It requires a valid name')))
+          .then(() => done(new Error("It requires a valid name")))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Pokemon.create({ name: 'Pikachu' });
+      it("should throw an error if height is not between 0 and 25", (done) => {
+        Pokemon.create({ height: 30 })
+          .then(() => done(new Error("It requires a value between 0 and 25")))
+          .catch(() => done());
+      });
+      it("should throw an error if weight is not between 0 and 1500", (done) => {
+        Pokemon.create({ weight: 1600 })
+          .then(() => done(new Error("It requires a value between 0 and 1500")))
+          .catch(() => done());
+      });
+      it("should throw an error if speed is not between 0 and 300", (done) => {
+        Pokemon.create({ speed: 305 })
+          .then(() => done(new Error("It requires a value between 0 and 300")))
+          .catch(() => done());
+      });
+      it("should throw an error if attack is not between 0 and 300", (done) => {
+        Pokemon.create({ attack: 305 })
+          .then(() => done(new Error("It requires a value between 0 and 300")))
+          .catch(() => done());
+      });
+      it("should throw an error if defense is not between 0 and 300", (done) => {
+        Pokemon.create({ defense: 1000 })
+          .then(() => done(new Error("It requires a value between 0 and 300")))
+          .catch(() => done());
+      });
+      it("should throw an error if hp is not between 0 and 300", (done) => {
+        Pokemon.create({ hp: -1 })
+          .then(() => done(new Error("It requires a value between 0 and 300")))
+          .catch(() => done());
+      });
+      it("should work when its a valid name", () => {
+        Pokemon.create({ name: "Pikachu" });
       });
     });
   });
--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -1,24 +1,130 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const { expect } = require('chai');
-const session = require('supertest-session');
-const app = require('../../src/app.js');
-const { Pokemon, conn } = require('../../src/db.js');
+const { expect } = require("chai");
+const session = require("supertest-session");
+const app = require("../../src/app.js");
+const { Pokemon, conn } = require("../../src/db.js");
 
 const agent = session(app);
-const pokemon = {
-  name: 'Pikachu',
-};
 
-describe('Pokemon routes', () => {
-  before(() => conn.authenticate()
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  }));
-  beforeEach(() => Pokemon.sync({ force: true })
-    .then(() => Pokemon.create(pokemon)));
-  describe('GET /pokemons', () => {
-    it('should get 200', () =>
-      agent.get('/pokemons').expect(200)
-    );
+describe("Pokemon routes", () => {
+  conn
+    .authenticate()
+    //.then(conn.sync({ force: true }))
+    .catch((err) => {
+      console.error("Unable to connect to the database:", err);
+    });
+
+  describe("GET/pokemons", () => {
+    it("should return status 200 ", () => {
+      try {
+        agent
+          .get("/pokemons")
+          .then((response) => response.statusCode.should.equal(200))
+          .catch(() => done());
+      } catch (error) {
+        done();
+      }
+    });
+
+    it("should return status 200 ", () => {
+      try {
+        agent
+          .get("/pokemons?name=Venusaur")
+          .then((response) => response.statusCode.should.equal(200))
+          .catch(() => done());
+      } catch (error) {
+        done();
+      }
+    });
+
+    it("should return status message ", () => {
+      try {
+        agent
+          .get("/pokemons?name=totoro")
+          .then((response) =>
+            response.text.toBe(
+              "El nombre ingresado no corresponde a ningun Pokemon"
+            )
+          )
+          .catch(() => done());
+      } catch (error) {
+        done();
+      }
+    });
+  });
+
+  describe("GET/pokemons/idPokemon", () => {
+    it("should return status 200 ", () => {
+      try {
+        agent
+          .get("/pokemons/2")
+          .then((response) => response.statusCode.should.equal(200))
+          .catch(() => done());
+      } catch (error) {
+        done();
+      }
+    });
+
+    it("should return status message ", () => {
+      try {
+        agent
+          .get("/pokemons/222222")
+          .then((response) =>
+            response.text.toBe(
+              "El ID ingresado no corresponde a ningun Pokemon"
+            )
+          )
+          .catch(() => done());
+      } catch (error) {
+        done();
+      }
+    });
+  });
+
+  describe("GET/types", () => {
+    it("should return status 200 ", () => {
+      try {
+        agent
+          .get("/types")
+          .then((response) => response.statusCode.should.equal(200))
+          .catch(() => done());
+      } catch (error) {
+        done();
+      }
+    });
+  });
+
+  describe("POST/pokemons", () => {
+    it("should return status 404 ", () => {
+      try {
+        agent
+          .post("/pokemons")
+          .then((response) => response.statusCode.should.equal(404))
+          .catch(() => done());
+      } catch (error) {
+        done();
+      }
+    });
+
+    it("should return status 201 ", () => {
+      try {
+        agent
+          .post("/pokemons")
+          .send({ name: "Pepe", attack: 80 })
+          .then((response) => response.statusCode.should.equal(201))
+          .catch(() => done());
+      } catch (error) {
+        done();
+      }
+    });
   });
 });
+
+/* it("should return status 200 ", async () => {
+      try {
+        const res = await agent.get("/pokemons");
+        expect(res.statusCode).toBe(200);
+      } catch (error) {
+        done();
+      }
+    });*/
